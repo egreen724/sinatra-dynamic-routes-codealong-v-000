ebbcb2f4beba10f95f30174a3dc325a06fe7f58d
Erin Green <erin.y.green@gmail.com>
2018-12-19 01:47:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,6 +17,9 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Goodbye #{@user_name}."
   end 
+  
+  get "/multiply/:num1/:num2" do 
+    @
   # Code your final two routes here:
 
 end
